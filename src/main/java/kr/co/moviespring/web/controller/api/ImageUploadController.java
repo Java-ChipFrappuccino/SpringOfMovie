1918e502396235d19e9c3f06d529a6751b45811b
Joon <pss21242124@gmail.com>
2024-06-12 02:05:31 +0900

    ver. 2.2.81 / 이미지 업로드 경로 수정

--- src/main/java/kr/co/moviespring/web/controller/api/ImageUploadController.java
+++ src/main/java/kr/co/moviespring/web/controller/api/ImageUploadController.java
@@ -3,6 +3,9 @@ package kr.co.moviespring.web.controller.api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +29,13 @@ public class ImageUploadController {
     @Value("${file.upload-dir}")
     private String uploadDir;
 
+    @Autowired
+    private Environment env; //현재 프로필값을 얻는 객체
+
+    private Boolean isProd() {
+        return env.acceptsProfiles("prod");
+    }
+
     @PostMapping(value = "/image-upload")
     // @RequestParam은 자바스크립트에서 설정한 이름과 반드시 같아야합니다.
     public ResponseEntity<?> imageUpload(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
@@ -33,13 +43,18 @@ public class ImageUploadController {
             // 서버에 저장할 경로
 //            String uploadDirectory = context.getServletContext().getRealPath(uploadDir);
             String uploadDirectory = Paths.get(uploadDir).toAbsolutePath().toString();
-            System.out.println(uploadDir);
+            if (isProd()==false) {
+                System.out.println("uploadDir : " + uploadDir);
+                System.out.println("uploadDirectory : " + uploadDirectory);
+                System.out.println(isProd());
+            }
+
 
             // 디렉토리가 존재하지 않으면 생성
             File dir = new File(uploadDirectory);
-            if (!dir.exists()) {
-                dir.mkdirs();
-            }
+//            if (!dir.exists()) {
+//                dir.mkdirs();
+//            }
 
             // 업로드 된 파일의 이름
             String originalFileName = file.getOriginalFilename();
@@ -54,18 +69,12 @@ public class ImageUploadController {
             file.transferTo(new File(uploadDirectory, uuidFileName));
 
             System.out.println("************************ 업로드 컨트롤러 실행 ************************");
-            System.out.println(uploadDirectory);
-//            String result = "/image/upload/" +uuidFileName;
+//            System.out.println(uploadDirectory);
             String imageUrl = "/image/upload/" + uuidFileName;
             // Ajax에서 업로드 된 파일의 이름을 응답 받을 수 있도록 해줍니다.
             return ResponseEntity.ok(imageUrl);
         } catch (Exception e) {
-//                        String uploadDirectory = Paths.get(uploadDir).toAbsolutePath().toString();
-//            System.out.println(uploadDirectory);
-            System.out.println(uploadDir);
-
             return ResponseEntity.badRequest().body("이미지 업로드 실패");
-
         }
 
     }
